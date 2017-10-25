229a8a520ce7fb0eb9f56a713e93137efcabc60b
fkfkfk9 <fkfkfk9@naver.com>
2017-10-25 09:44:36 +0900

    grouping_set chang

---
+++ 2017/oracle/10_24/create_classes_department.sql
@@ -0,0 +1,33 @@
+drop table classes;
+drop table department;
+
+create table department(
+	dep_code 	NUMBER primary key,
+	dep_name	varchar2(50) NOT NULL,
+	dean_name	varchar2(20),
+	dep_call	varchar2(20),
+	create_date	date NOT NULL
+);
+create table classes(
+	st_code		number primary key,
+	st_name		varchar2(20) NOT NULL,
+	dep_code	number REFERENCES department(dep_code),
+	st_gender	varchar2(20) CHECK ( st_gender IN ('남자', '여자')),
+	st_phone	varchar2(20),
+	st_birth	date,
+	st_addr		varchar2(50)
+);
+
+insert into department
+values (100, '컴퓨터 공학과', '홍길동', '000-000-0000', '1998-11-03');
+insert into department (dep_code, dep_name, dep_call, create_date)
+values (200, '모바일 소프트웨어', '010-001-1111', '2007-03-01');
+insert into department (dep_code, dep_name, dean_name, create_date)
+values (300, '정보보호', '알파고', sysdate);
+insert into classes
+values (20070001, '유종현', '200', '남자', '010-4590-5395','1988-11-03',
+'경기도 안양시 동안구');
+insert into classes (st_code, st_name, dep_code, st_gender)
+values (20070002, '박지성', '100', '남자');
+insert into classes (st_code, st_name, dep_code, st_gender, st_birth, st_addr)
+values (20070003, '김태희', '200', '여자', '1980-01-01','서울시');
--- 2017/oracle/10_24/grouping_sets.sql
+++ 2017/oracle/10_24/grouping_sets.sql
@@ -3,29 +3,28 @@
     GROUP BY writer + GROUP BY readnum 각각 GROUP BY를 시켜 더해준 값
 */
  
- select salary, job_id, sum(salary), count(*)
- from employees
- group by GROUPING SETS(salary, job_id)--각각 그룹바이한 결과를 유니온 올 한것과 같다.
- order by salary, job_id;
+ select dep_code, st_level, avg(st_grade), count(*)
+ from classes
+ group by GROUPING SETS(dep_code, st_level);--각각 그룹바이한 결과를 유니온 올 한것과 같다.
+ 
+select dep_code, avg(st_grade), count(*)
+ from classes
+ group by dep_code
+ order by dep_code;
+ --위 아래 결과를 합친것은 GROUPING SETS(dep_code, st_level) 같다.
+ select st_level, avg(st_grade), count(*)
+ from classes
+ group by st_level
+ order by st_level;
 
-select salary, sum(salary), count(*)
- from employees
- group by salary
- order by salary;
- --위 아래 결과를 합친것은 GROUPING SETS(salary, job_id) 같다.
- select job_id, sum(salary), count(*)
- from employees
- group by job_id
- order by job_id;
 
-
- select salary, job_id, DEPARTMENT_ID, sum(salary), count(*)
- from employees
- group by GROUPING SETS(salary,(job_id, DEPARTMENT_ID))
- order by salary, job_id;
---셀러리로만 그룹바이 한것과 아래것의 결과를 합친(유니온 올)것과 같다
-select job_id, DEPARTMENT_ID, sum(salary), count(*)
- from employees
- group by job_id, DEPARTMENT_ID
- order by job_id, DEPARTMENT_ID;
+ select st_gender, dep_code, st_level, avg(st_grade), count(*)
+ from classes
+ group by GROUPING SETS(st_gender, (dep_code, st_level))
+ order by st_gender, dep_code;
+--성별로만 그룹바이 한것과 아래것의 결과를 합친(유니온 올)것과 같다
+select dep_code, st_level, avg(st_grade), count(*)
+ from classes
+ group by dep_code, st_level
+ order by dep_code, st_level;
 --------------------------------------------------------------------------------
